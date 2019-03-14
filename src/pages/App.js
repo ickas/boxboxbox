556965afb6dfc724afc69a46e9c0b3805e2b5744
Henrique Macedo <mail@henriquemacedo.pt>
2019-03-14 22:07:38 +0000

    Added grid style

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,18 +3,32 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const CalendarWrapper = styled.div`
-  border: 1px solid red;
   width: 100vw;
-  /* min-height: 100vh; */
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  /* grid-template-rows: minmax(20vw, 1fr); */
+  grid-gap: 20px;
+  padding: 20px;
 
   > div {
-    border: 1px solid blue;
-    padding: 10%;
-    /* box-sizing: border-box; */
-    /* min-height: 20vw; */
+    /* background-color: var(--dark-grey, #15151e); */
+    border: 10px solid #38383f;
+    border-left: 0;
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: 25px;
+    /* border-width: 0 15px;
+    border-style: solid;
+    border-color: var(--red, #e10600);
+    border-radius: 25px; */
+    padding: 20px;
+
+    &.active {
+      border-color: var(--red, #e10600);
+    }
+
+    h1 {
+      font-size: 1rem;
+      font-weight: 700;
+    }
   }
 `;
 
@@ -22,7 +36,14 @@ class App extends Component {
   render() {
     return (
       <CalendarWrapper>
-        <div>1</div>
+        <div className="active">
+          <h1>FORMULA 1 ROLEX AUSTRALIAN GRAND PRIX 2019</h1>
+          <p>17 Mar / 05:10 - 07:10 / Race</p>
+          <p>16 Mar / 06:00 - 07:00 / Qualifying</p>
+          <p>16 Mar / 03:00 - 04:00 / Practice 3</p>
+          <p>15 Mar / 05:00 - 06:30 / Practice 2</p>
+          <p>15 Mar / 01:00 - 02:30 / Practice 1</p>
+        </div>
         <div>2</div>
         <div>3</div>
         <div>4</div>
