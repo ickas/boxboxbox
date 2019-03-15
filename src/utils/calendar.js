43d2aaa3ba4b6feef6680d5dbefe6cd13e174540
Henrique Macedo <mail@henriquemacedo.pt>
2019-03-15 20:54:21 +0000

    Added calendar util

---
+++ src/utils/calendar.js
@@ -0,0 +1,43 @@
+const calendar = [
+  {
+    key: 1,
+    name: "FORMULA 1 ROLEX AUSTRALIAN GRAND PRIX 2019",
+    race: {
+      day: 17,
+      month: "mar",
+      phase: "Race",
+      startTime: "05:10",
+      endTime: "07:10"
+    },
+    quali: {
+      day: 16,
+      month: "mar",
+      phase: "Qualifying",
+      startTime: "06:00",
+      endTime: "07:00"
+    },
+    fp3: {
+      day: 16,
+      month: "mar",
+      phase: "Practice 3",
+      startTime: "03:00",
+      endTime: "04:00"
+    },
+    fp2: {
+      day: 15,
+      month: "mar",
+      phase: "Practice 2",
+      startTime: "05:00",
+      endTime: "06:30"
+    },
+    fp1: {
+      day: 15,
+      month: "mar",
+      phase: "Practice 1",
+      startTime: "01:00",
+      endTime: "02:30"
+    }
+  }
+];
+
+export default calendar;
